7e1be3c3b8787d0b18442412a2ce3d83824bab02
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-31 15:32:20 -0300

    Brew formula update for clockify-cli version v0.12.1

--- Formula/clockify-cli.rb
+++ Formula/clockify-cli.rb
@@ -2,16 +2,16 @@
 class ClockifyCli < Formula
   desc "Helps to interact with Clockfy's API"
   homepage "https://github.com/lucassabreu/clockify-cli"
-  version "0.12.0"
+  version "0.12.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.12.0/clockify-cli_0.12.0_Darwin_x86_64.tar.gz"
-    sha256 "5a41ccf4dfaf36b73308b98245b83d5d8f4f811c70115dbbccdfafa7d4287a4f"
+    url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.12.1/clockify-cli_0.12.1_Darwin_x86_64.tar.gz"
+    sha256 "f77ee767b12af86e9b59b2c9f2a3088a1cdf98a6ff8566405c074d665978e658"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.12.0/clockify-cli_0.12.0_Linux_x86_64.tar.gz"
-      sha256 "b1b947eb5e893b583edf267cb25c075810d0e387636c9b94519ed9e7b9a9a634"
+      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.12.1/clockify-cli_0.12.1_Linux_x86_64.tar.gz"
+      sha256 "a7321b31fee128a7d89bd4137efe8c1aa2ec204fcc9658c46811d446d1147829"
     end
   end
 

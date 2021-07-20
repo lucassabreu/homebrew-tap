823b1bf9dc0df59888d418efd50c31795b80fa99
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-20 08:39:18 -0300

    Brew formula update for clockify-cli version v0.19.2

--- Formula/clockify-cli.rb
+++ Formula/clockify-cli.rb
@@ -5,28 +5,28 @@
 class ClockifyCli < Formula
   desc "Helps to interact with Clockfy's API"
   homepage "https://github.com/lucassabreu/clockify-cli"
-  version "0.19.1"
+  version "0.19.2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.1/clockify-cli_0.19.1_Darwin_x86_64.tar.gz"
-      sha256 "78d70082221d614a80dcd5e340c9678c73c281878dbe3aa5993c903b48cba3b8"
+      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.2/clockify-cli_0.19.2_Darwin_x86_64.tar.gz"
+      sha256 "6a73b83ba155e671839e381633e85312b13de718e2a75f8b1dde6bb82c7842ac"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.1/clockify-cli_0.19.1_Darwin_arm64.tar.gz"
-      sha256 "0885dee3c60c452b4e867fb85299046841c8c6f596b5133c29bfca363c18d2f8"
+      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.2/clockify-cli_0.19.2_Darwin_arm64.tar.gz"
+      sha256 "065a05dbba404612cf34249c5fdae391f41bf8659e221f4284d9632cad12f8ce"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.1/clockify-cli_0.19.1_Linux_x86_64.tar.gz"
-      sha256 "d6cc558cc47d43ff1af319f3c596521eb64d7720f1e40b2eb0a3aa63fbceb820"
+      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.2/clockify-cli_0.19.2_Linux_x86_64.tar.gz"
+      sha256 "feff876a3a165e63bc113548c4390cdbe6e57d02bcf78fbd43e15326992cdfb9"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.1/clockify-cli_0.19.1_Linux_arm64.tar.gz"
-      sha256 "9de4206e94174514e354e461cfec031ad872d043527aeba9b705978b25c44be3"
+      url "https://github.com/lucassabreu/clockify-cli/releases/download/v0.19.2/clockify-cli_0.19.2_Linux_arm64.tar.gz"
+      sha256 "d32bca357786ff81616838155dff22c51ab321cde17da00fa68ae4496e8554b7"
     end
   end
 
